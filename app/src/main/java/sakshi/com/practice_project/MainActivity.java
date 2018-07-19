7f2d1923c2ddd666d60b8772cb055eb8407e7313
Prajna Shetty <prajnashetty340@gmail.com>
2018-07-19 14:22:15 +0530

    Prajna commits

--- app/src/main/java/sakshi/com/practice_project/MainActivity.java
+++ app/src/main/java/sakshi/com/practice_project/MainActivity.java
@@ -1,13 +1,66 @@
 package sakshi.com.practice_project;
-
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button next, info;
+  TextView name, email, mobile;
+    String json_url="http://192.168.1.107/getinfo.php";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        next = (Button) findViewById(R.id.bt_next);
+        info = (Button) findViewById(R.id.bt_info);
+        name = (TextView) findViewById(R.id.tv_name);
+        email = (TextView) findViewById(R.id.tv_mail);
+        mobile = (TextView) findViewById(R.id.tv_mobile);
+
+
+        bt_info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, json_url,null,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                try {
+                                    tv_name.setText(response.getString("Name"));
+                                    tv_mail.setText(response.getString("Email"));
+                                    tv_mobile.setText(response.getString("Mobile"));
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, "Somethings wrong", Toast.LENGTH_SHORT).show();
+                        error.printStackTrace();
+                    }
+                });
+                MySingleton.getInstance(MainActivity.this).addToRequestque(jsonObjectRequest);
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/sakshi/com/practice_project/MySingleton.java
@@ -0,0 +1,39 @@
+package sakshi.com.practice_project;
+
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class MySingleton {
+    private static  MySingleton mInstance;
+    private RequestQueue requestQueue;
+    private static Context mCtx;
+
+
+    private  MySingleton(Context context){
+        mCtx=context;
+        requestQueue=getRequestque();
+    }
+
+    public RequestQueue getRequestque(){
+        if(requestQueue== null)
+            requestQueue= Volley.newRequestQueue(mCtx.getApplicationContext());
+        return  requestQueue;
+    }
+
+
+    public  static  synchronized  MySingleton getInstance(Context context){
+        if(mInstance== null){
+
+            mInstance= new MySingleton(context);
+        }
+        return mInstance;
+    }
+    public<T> void addToRequestque(Request<T> request){
+        requestQueue.add(request);
+
+    }
+}
+
