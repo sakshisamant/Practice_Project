b8bca56551ede60b927c4f6051c1582ab714e545
sakshi <sakshisamant79@gmail.com>
2018-07-19 13:44:18 +0530

    3 Textboxes and 2 buttons done!

--- app/src/main/java/sakshi/com/practice_project/MainActivity.java
+++ app/src/main/java/sakshi/com/practice_project/MainActivity.java
@@ -8,18 +8,11 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
 
-    Button next, info;
-    TextView name, email, mobile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        next = (Button) findViewById(R.id.bt_next);
-        info = (Button) findViewById(R.id.bt_info);
-        name = (TextView) findViewById(R.id.tv_name);
-        email = (TextView) findViewById(R.id.tv_mail);
-        mobile = (TextView) findViewById(R.id.tv_mobile);
 
 
     }
